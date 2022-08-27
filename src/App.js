206827dc2d6dc18f198f16111cd63c0fd88c171e
asamnt <amitsamant@gmail.com>
2022-08-27 21:50:04 +0530

    added meals summary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from "./components/layout/Header";
 import { Fragment } from "react";
+import Meals from "./components/meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header/>
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
